7cc9e612398e162f7bfbd3f7de5a7472ed3bfd7d
LordSantanna <adriacabeza@hotmail.com>
2018-08-10 12:29:53 +0200

    Starting to write training

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ import numpy as np
 import pickle
 from matplotlib import pyplot
 
-
-
-
-
 # UTIL FUNCTIONS
 def sigmoid(x, derivative = False):
     if(derivative == True):
@@ -58,110 +54,98 @@ class RNN(nn.Module):
         return out
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     #HYPER-PARAMETERS
-#     sequence_length = 28
-#     input_size = 1 
-#     hidden_size = 128
-#     num_layers = 1
-#     batchsize = 1 #number of sequences I want to process in parallel
-#     num_epochs = 1 #train the data 1 time
-#     learning_rate = 0.01 #learning rate
+    #HYPER-PARAMETERS
+    sequence_length = 28
+    input_size = 4
+    hidden_size = 128
+    num_layers = 1
+    batchsize = 1 #number of sequences I want to process in parallel
+    num_epochs = 1 #train the data 1 time
+    learning_rate = 0.01 #learning rate
 
-#     model = RNN(input_size, hidden_size, num_layers)
-#     #Loss, optimizer
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    model = RNN(input_size, hidden_size, num_layers)
+    #Loss, optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-#     for epoch in range(num_epochs):
-#         for i , data in enumerate(train_loader,0):
+    #begin to train 
+    for epoch in range(15):#gotta change 15 by num_epochs
+        print('STEP: ', i)
+        optimizer.zero_grad()
+        train_data,train_position,test_data,test_position = load_data_sets()
+        print('Train data size', train_data.size())
+        print('Train output size',train_position.size())
+        print('Test data size', test_data.size())
+        print('Test output size',test_position.size())
 
-#                 inputs, labels = data
-#                 inputs = Variable(inputs.view(-1, batch_size, input_size))
-#                 labels = Variable(labels.view(-1, batch_size, input_size))
+        state = Variable(torch.zeros(num_layer,batch_size,hidden_size))
+        cell = Variable(torch.zeros(num_layer,batch_size,hidden_size))
+        out, state = RNN(train_data, state, cell)
 
-#                 print('Input/Label Size :::: ', inputs.size(), labels.size())
+        
+        out = out.view(-1, hidden_size)
+        labels = labels.view(-1).long()
 
-#                 state = Variable(torch.zeros(num_layer,batch_size,hidden_size))
-#                 cell = Variable(torch.zeros(num_layer,batch_size,hidden_size))
+                print('Output/Label Size :::: ', out.size(), labels.size())
 
-#                 out, state = RNN(inputs, state, cell)
+                loss = nn.CrossEntropyLoss()
+                err = loss(out, labels)
+                err.backward()
+                optimizer.step()
 
-#                 optimizer.zero_grad()
-
-#                 out = out.view(-1, hidden_size)
-#                 labels = labels.view(-1).long()
-
-#                 print('Output/Label Size :::: ', out.size(), labels.size())
-
-#                 loss = nn.CrossEntropyLoss()
-#                 err = loss(out, labels)
-#                 err.backward()
-#                 optimizer.step()
-
-#                 print('[input]', inputs.view(1,-1))
-#                 print('[target]', labels.view(1,-1))
-#                 print('[prediction] ', out.data.max(1)[1])
-
-
-#     print('-------done')
+                print('[input]', inputs.view(1,-1))
+                print('[target]', labels.view(1,-1))
+                print('[prediction] ', out.data.max(1)[1])
 
 
-# def load_train_test():
+    print('-------done')
 
 
-filename = "train.p"
-train_list= pickle.load(open(filename , "rb" ))
-train_data=[]
-train_position=[]
-for event in train_list:
-    for moment in event.moments:
-        for player in moment.players:
-            train_data.append(player.get_info())
-            train_position.append(player.x)
-            train_position.append(player.y)
+def load_data_sets():
+    filename = "train.p"
+    train_list= pickle.load(open(filename , "rb" ))
+    train_data=[]
+    train_position=[]
+    for event in train_list:
+        for moment in event.moments:
+            for player in moment.players:
+                train_data.append(player.get_info())
+                train_position.append(player.x)
+                train_position.append(player.y)
 
-#has teamid,x,y for every player for every moment       
-train_data = grouplen(train_data,4)
-torch.set_printoptions(precision=8)
-train_data = torch.tensor(np.array(train_data))
-train_data = Variable(train_data)
-#train_position has every player position every event
-train_position = grouplen(train_position,2)
-train_position = torch.tensor(np.array(train_position))
-train_position = Variable(train_position)
+    #has team,id,x,y for every player for every moment       
+    train_data = grouplen(train_data,4)
+    torch.set_printoptions(precision=8)
+    train_data = torch.tensor(np.array(train_data))
+    train_data = Variable(train_data)
+    #train_position has every player position every event
+    train_position = grouplen(train_position,2)
+    train_position = torch.tensor(np.array(train_position))
+    train_position = Variable(train_position)
 
+    data = pickle.load(open("test_data.p" , "rb" ))
+    for j in range(151):
+        test_data=[]
+        test_position=[]        
+        for i in range(6):
+            for obj in vars(data[j][i])["players"]:
+                test_data.append(obj.get_info())
+                test_position.append(player.x)
+                test_position.append(player.y)
 
-data = pickle.load(open("test_data.p" , "rb" ))
-for j in range(151):
-    test_data=[]
-    test_position=[]        
-    for i in range(6):
-        for obj in vars(data[j][i])["players"]:
-            test_data.append(obj.get_info())
-            test_position.append(player.x)
-            test_position.append(player.y)
+    #test_Data has team,id,x,y for every player for every moment with (-1,-1)       
+    test_data = grouplen(test_data,4)
+    test_data = torch.tensor(np.array(test_data))
+    test_data = Variable(test_data)
 
-#test_Data has teamid,x,y for every player for every moment with (-1,-1)       
-test_data = grouplen(test_data,4)
-test_data = torch.tensor(np.array(test_data))
-test_data = Variable(test_data)
+    #test_position has x,y for every player for every moment with (-1,-1)
+    test_position = grouplen(test_position,2)
+    test_position = torch.tensor(np.array(test_position))
+    test_position = Variable(test_position)
 
-#test_position has x,y for every player for every moment with (-1,-1)
-test_position = grouplen(test_position,2)
-test_position = torch.tensor(np.array(test_position))
-test_position = Variable(test_position)
-
-# return train_data,train_position,test_data,test_position
-
-
-
-  
-#     # #TODO: fer lo mateix per train i mirar que polles hi ha 
-#     # 
-
-
+    return train_data,train_position,test_data,test_position
 
 # gt_data = pickle.load(open("gt_data.p", "rb"))
 # data = pickle.load(open("test_data.p" , "rb" ))
@@ -193,4 +177,3 @@ test_position = Variable(test_position)
 # #                 print 'Jugador encontrado que tengo que predecir ', player.get_info()
 # #         i = i+ 1
  
-
